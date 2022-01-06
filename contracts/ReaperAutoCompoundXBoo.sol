b81be0ddb70e624781e0b3f8f8f540de7577127e
Magnus Brantheim <m.brantheim@gmail.com>
2022-01-06 19:49:52 +0000

    Implemented new fee structure in BaseStrategy

--- contracts/ReaperAutoCompoundXBoo.sol
+++ contracts/ReaperAutoCompoundXBoo.sol
@@ -56,29 +56,6 @@ contract ReaperAutoCompoundXBoo is ReaperBaseStrategy {
     address public strategist;
 
     /**
-     * @dev Distribution of fees earned. This allocations relative to the % implemented on
-     * Current implementation separates 5% for fees. Can be changed through the constructor
-     * Inputs in constructor should be ratios between the Fee and Max Fee, divisble into percents by 10000
-     *
-     * {callFee} - Percent of the totalFee reserved for the harvester (1000 = 10% of total fee: 0.5% by default)
-     * {treasuryFee} - Percent of the totalFee taken by maintainers of the software (9000 = 90% of total fee: 4.5% by default)
-     * {securityFee} - Fee taxed when a user withdraws funds. Taken to prevent flash deposit/harvest attacks.
-     * These funds are redistributed to stakers in the pool.
-     *
-     * {totalFee} - divided by 10,000 to determine the % fee. Set to 5% by default and
-     * lowered as necessary to provide users with the most competitive APY.
-     *
-     * {MAX_FEE} - Maximum fee allowed by the strategy. Hard-capped at 5%.
-     * {PERCENT_DIVISOR} - Constant used to safely calculate the correct percentages.
-     */
-
-    uint256 public callFee = 1000;
-    uint256 public treasuryFee = 9000;
-    uint256 public securityFee = 10;
-    uint256 public totalFee = 450;
-    uint256 public constant MAX_FEE = 500;
-
-    /**
      * @dev Routes we take to swap tokens
      * {wftmToBooRoute} - Route we take to get from {wftm} into {boo}.
      * {poolRewardToWftmPaths} - Routes for each pool to get from {pool reward token} into {wftm}.
@@ -111,12 +88,8 @@ contract ReaperAutoCompoundXBoo is ReaperBaseStrategy {
     mapping(uint8 => uint256) public poolxBooBalance;
 
     /**
-     * {TotalFeeUpdated} Event that is fired each time the total fee is updated.
-     * {CallFeeUpdated} Event that is fired each time the call fee is updated.
      * {UpdatedStrategist} Event that is fired each time the strategist role is updated.
      */
-    event TotalFeeUpdated(uint256 newFee);
-    event CallFeeUpdated(uint256 newCallFee, uint256 newTreasuryFee);
     event UpdatedStrategist(address newStrategist);
 
     /**
@@ -588,30 +561,6 @@ contract ReaperAutoCompoundXBoo is ReaperBaseStrategy {
                 0
             );
         }
-    }
-
-    /**
-     * @dev updates the total fee, capped at 5%
-     */
-    function updateTotalFee(uint256 _totalFee)
-        external
-        onlyOwner
-        returns (bool)
-    {
-        require(_totalFee <= MAX_FEE, "Fee Too High");
-        totalFee = _totalFee;
-        emit TotalFeeUpdated(totalFee);
-        return true;
-    }
-
-    /**
-     * @dev updates the call fee and adjusts the treasury fee to cover the difference
-     */
-    function updateCallFee(uint256 _callFee) external onlyOwner returns (bool) {
-        callFee = _callFee;
-        treasuryFee = PERCENT_DIVISOR.sub(callFee);
-        emit CallFeeUpdated(callFee, treasuryFee);
-        return true;
     }
 
     /**
--- contracts/abstract/ReaperBaseStrategy.sol
+++ contracts/abstract/ReaperBaseStrategy.sol
@@ -14,6 +14,44 @@ abstract contract ReaperBaseStrategy is Pausable, Ownable {
     uint256 public constant PERCENT_DIVISOR = 10_000;
     uint256 public constant ONE_YEAR = 365 days;
 
+    /**
+     * {TotalFeeUpdated} Event that is fired each time the total fee is updated.
+     * {FeesUpdated} Event that is fired each time callFee+treasuryFee+strategistFee are updated.
+     */
+    event TotalFeeUpdated(uint256 newFee);
+    event FeesUpdated(
+        uint256 newCallFee,
+        uint256 newTreasuryFee,
+        uint256 newStrategistFee
+    );
+
+    /**
+     * @dev Distribution of fees earned. This allocations relative to the % implemented on
+     * Current implementation separates 5% for fees. Can be changed through the constructor
+     * Inputs in constructor should be ratios between the Fee and Max Fee, divisble into percents by 10000
+     *
+     * {callFee} - Percent of the totalFee reserved for the harvester (1000 = 10% of total fee: 4.5% by default)
+     * {treasuryFee} - Percent of the totalFee taken by maintainers of the software (6500 = 65% of total fee: 4.5% by default)
+     * {strategistFee} - Percent of the totalFee taken by strategist (2500 = 25% of total fee: 4.5% by default)
+     * {securityFee} - Fee taxed when a user withdraws funds. Taken to prevent flash deposit/harvest attacks.
+     * These funds are redistributed to stakers in the pool.
+     *
+     * {totalFee} - divided by 10,000 to determine the % fee. Set to 4.5% by default and
+     * lowered as necessary to provide users with the most competitive APY.
+     *
+     * {STRATEGIST_MAX_FEE} - Maximum strategist fee allowed by the strategy. Hard-capped at 50%.
+     * {MAX_FEE} - Maximum fee allowed by the strategy. Hard-capped at 10%.
+     * {PERCENT_DIVISOR} - Constant used to safely calculate the correct percentages.
+     */
+
+    uint256 public callFee = 1000;
+    uint256 public treasuryFee = 6500;
+    uint256 public strategistFee = 2500;
+    uint256 public securityFee = 10;
+    uint256 public totalFee = 450;
+    uint256 public constant STRATEGIST_MAX_FEE = 5000;
+    uint256 public constant MAX_FEE = 1000;
+
     struct Harvest {
         uint256 timestamp;
         uint256 profit;
@@ -161,6 +199,40 @@ abstract contract ReaperBaseStrategy is Pausable, Ownable {
         onlyOwner
     {
         harvestLogCadence = _newCadenceInSeconds;
+    }
+
+    /**
+     * @dev updates the total fee, capped at 10%
+     */
+    function updateTotalFee(uint256 _totalFee)
+        external
+        onlyOwner
+        returns (bool)
+    {
+        require(_totalFee <= MAX_FEE, "Fee Too High");
+        totalFee = _totalFee;
+        emit TotalFeeUpdated(totalFee);
+        return true;
+    }
+
+    function updateFees(
+        uint256 _callFee,
+        uint256 _treasuryFee,
+        uint256 _strategistFee
+    ) external onlyOwner returns (bool) {
+        require(
+            _strategistFee <= STRATEGIST_MAX_FEE,
+            "Cannot set the strategist fee higher than max fee"
+        );
+        require(
+            _callFee.add(_treasuryFee).add(_strategistFee) == PERCENT_DIVISOR,
+            "Fees must add up to 100%"
+        );
+        callFee = _callFee;
+        treasuryFee = _treasuryFee;
+        strategistFee = _strategistFee;
+        emit FeesUpdated(callFee, treasuryFee, strategistFee);
+        return true;
     }
 
     /**
